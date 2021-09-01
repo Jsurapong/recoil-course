3419ed03b7a42c56b0a0e89138473e13fccd05e3
surapong <surapong@enres.co>
2021-09-01 13:24:41 +0700

    setup prettierrc

--- src/components/Rectangle/Rectangle.tsx
+++ src/components/Rectangle/Rectangle.tsx
@@ -1,29 +1,29 @@
-import {useState} from 'react'
-import {RectangleContainer} from './RectangleContainer'
-import {RectangleInner} from './RectangleInner'
+import { useState } from "react";
+import { RectangleContainer } from "./RectangleContainer";
+import { RectangleInner } from "./RectangleInner";
 
 export type ElementStyle = {
-    position: {top: number; left: number}
-    size: {width: number; height: number}
-}
+  position: { top: number; left: number };
+  size: { width: number; height: number };
+};
 
 export const Rectangle = () => {
-    const [element] = useState({
-        style: {
-            position: {top: 100, left: 100},
-            size: {width: 100, height: 100},
-        },
-    })
+  const [element] = useState({
+    style: {
+      position: { top: 100, left: 100 },
+      size: { width: 100, height: 100 },
+    },
+  });
 
-    return (
-        <RectangleContainer
-            position={element.style.position}
-            size={element.style.size}
-            onSelect={() => {
-                console.log("I've been selected!")
-            }}
-        >
-            <RectangleInner selected={false} />
-        </RectangleContainer>
-    )
-}
+  return (
+    <RectangleContainer
+      position={element.style.position}
+      size={element.style.size}
+      onSelect={() => {
+        console.log("I've been selected!");
+      }}
+    >
+      <RectangleInner selected={false} />
+    </RectangleContainer>
+  );
+};
